acef9c60fb4e95868b5d8b9b6be5dea1c8fc964d
sonysunny0007 <43338156+sonysunny0007@users.noreply.github.com>
2025-08-29 10:31:33 -0600

    Started developing About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,20 @@
-export default function About() {
-  return (
-        <h1>About</h1>
-  );
+// src/components/about.tsx
+import Image from "next/image";
+
+export default function AboutPage() {
+  return(
+    <>
+  <main className="min-h-screen bg-black text-white" id="about">
+    <section className="flex flex-col justify-center items-center text-center py-20 px-6">
+      <h1 
+        className=""
+      
+      >
+        Sony Sunny
+      </h1>
+
+    </section>
+  </main>
+  </>
+  )
 }
-
-
