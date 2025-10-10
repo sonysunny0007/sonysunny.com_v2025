a3fbfab4976422ba4cd56b735eb035283fe8f938
sonysunny0007 <43338156+sonysunny0007@users.noreply.github.com>
2025-10-10 15:10:34 -0600

    added output:export for the deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enables static HTML export for Netlify
+  images: {
+    unoptimized: true, // ✅ Disable Image Optimization for static export
+  },
 };
 
 export default nextConfig;
