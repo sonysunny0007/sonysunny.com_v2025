528e8dc58e665db5275d6f9275bb3b3488707b5b
sonysunny0007 <43338156+sonysunny0007@users.noreply.github.com>
2025-10-10 15:12:02 -0600

    Made changes in the main page for the blog and store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 //import About from "@/components/About";
 import Services from "@/components/Services";
 import Projects from "@/components/Projects";
-import Footer from "@/components/Footer";
 import Testimonials from "@/components/testimonial";
 import Blog from "@/components/blog";
 import Store from "@/components/store";
