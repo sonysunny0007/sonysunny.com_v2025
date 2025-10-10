b9e25640f179cefeffd320a7c4e147fdafd193a3
sonysunny0007 <43338156+sonysunny0007@users.noreply.github.com>
2025-10-10 15:15:30 -0600

    Added contents to the testimonial

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -7,28 +7,28 @@ const testimonials = [
     role: "Experienced Software Engineer",
     image: "/joel.jpeg", // put your image in public/ folder
     rating: 4.5,
-    text: "It has been an absolute pleasure working with Relume. The team went above and beyond to help us execute on our vision and always.",
+    text: "Collaborating with Sony Sunny has been an absolute pleasure. Their professionalism, creativity, and technical insight make every project seamless and impactful.",
   },
   {
     name: "Jenu James",
     role: "Embedded Software Developer",
     image: "/jenu.jpeg",
-    rating: 4.5,
-    text: "It has been an absolute pleasure working with Relume. The team went above and beyond to help us execute on our vision and always.",
+    rating: 4.95,
+    text: "Working with Sony Sunny has been an exceptional experience. Their deep understanding of technology and attention to detail made collaboration on complex firmware projects smooth and highly productive.",
   },
   {
     name: "Oleg Serbinov",
     role: "Lead Hardware Design Engineer",
     image: "/oleg.jpeg",
-    rating: 4.5,
-    text: "It has been an absolute pleasure working with Relume. The team went above and beyond to help us execute on our vision and always.",
+    rating: 4.75,
+    text: "Partnering with SonySunny has truly elevated our hardware projects. Their expertise in hardware design and commitment to precise, innovative solutions made every project efficient and rewarding.",
   },
   {
     name: "Clara Mike",
     role: "Fashion Designer",
     image: "/fashion_designer.png",
-    rating: 4.5,
-    text: "It has been an absolute pleasure working with Relume. The team went above and beyond to help us execute on our vision and always.",
+    rating: 5,
+    text: "Working with Sony Sunny has been a wonderful experience. They brought my vision to life with creativity, attention to detail, and a seamless website design that truly represents my brand.",
   },
 ];
 
